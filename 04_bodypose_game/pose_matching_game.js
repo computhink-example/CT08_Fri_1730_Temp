c15d3e82a8bb7ddda45cc6989c8d9d8ea098335e
computhink-example <computhink-example@outlook.com>
2026-09-25 10:11:36 +0000

    Sep 25, 2026, 10:11 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -26,6 +26,7 @@ let rightPanelX = sidePanelWidth + cameraWidth;
 // Setup variables
 let video;
 let bodyPose; // ML Model
+let detectedPeople 
 
 // ====================================================
 // Preload
