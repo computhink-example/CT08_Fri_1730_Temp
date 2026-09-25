88d26b728c2def0de23d1bffd7a77b167576c313
computhink-example <computhink-example@outlook.com>
2026-09-25 10:14:53 +0000

    Sep 25, 2026, 10:14 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -132,3 +132,7 @@ function drawMiddleLine() {
     // Draw the middle line inside the webcam area.
     line(width / 2, 0, width / 2, cameraHeight);
 }
+
+// ====================================================
+// Draw middle divider line
+// ====================================================
