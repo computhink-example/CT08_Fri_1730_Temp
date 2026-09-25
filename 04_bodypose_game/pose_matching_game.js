0afc5955c8e42790b72c4afa06bc006427b15326
computhink-example <computhink-example@outlook.com>
2026-09-25 10:48:02 +0000

    Sep 25, 2026, 10:48 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -163,7 +163,7 @@ function drawDetectionStatus() {
 
 // Draw line between two body points
 function drawBodyLine(point1, point2) {
-    line(); // line(x pos 1, y pos 1, )
+    line(); // line(x pos 1, y pos 1, x pos 2, y pos 2)
 }
 
 // Draw circle on body point
