f7e418f360b9cacbd531c97186bf035fff69cf25
computhink-example <computhink-example@outlook.com>
2026-09-25 09:59:46 +0000

    Sep 25, 2026, 9:59 AM

--- 04_bodypose_game/pose_matching_game.js
+++ 04_bodypose_game/pose_matching_game.js
@@ -23,6 +23,7 @@ let leftPanelX = 0;
 // x-position of the right panel.
 let rightPanelX = sidePanelWidth + cameraWidth;
 
+// Setup variables
 let video;
 
 // ====================================================
